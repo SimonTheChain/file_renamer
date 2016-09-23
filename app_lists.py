efa9c764b9f7f2c04388fad9a712de722b98a5e4
SimonTheChain <simonthechain@gmail.com>
2016-09-22 21:13:29 -0400

    Created another file for the lists Continued coding the app

---
+++ app_lists.py
@@ -0,0 +1,55 @@
+MA_RESOLUTIONS = [
+    "1920x1080",
+    "1280x720",
+]
+
+
+MEZZ_RESOLUTIONS = [
+    "1280x720",
+    "NTSC 16:9 SD",
+    "NTSC 4:3 SD",
+]
+
+
+CUSTOM_RESOLUTIONS = [
+    ("1920x1080", "1080", "hd"),
+    ("1280x720", "720", "hd720"),
+    ("NTSC 16:9 SD", "NTSC16", "NTSC16"),
+    ("NTSC 4:3 SD", "NTSC43", "NTSC43"),
+    ("PAL 16:9 SD", "PAL16", "PAL16"),
+    ("PAL 4:3 SD", "PAL43", "PAL43"),
+    ("4K", "4K", "4K"),
+    ("2K", "2K", "2K"),
+    ("UHD", "UHD", "UHD"),
+]
+
+
+CUSTOM_EXTENSIONS = [
+    ".mov",
+    ".mxf",
+    ".mp4",
+    ".mpeg",
+    ".wav",
+    ".aiff",
+    ".scc",
+    ".itt",
+    ".stl",
+    ".xml",
+    ".txt",
+    ".doc",
+    ".docx",
+    ".srt",
+    ".cap",
+    ".aaf",
+    ".ult",
+    ".smi",
+    ".asc",
+    ".xls",
+    ".xlsx",
+    ".jpg",
+    ".png",
+    ".tif",
+    ".psd",
+    ".tga",
+    ".pdf",
+]
--- file_renamer.py
+++ file_renamer.py
@@ -7,28 +7,19 @@
 
 
 import sys
+from collections import OrderedDict
 from PyQt4 import QtCore, QtGui
 
 import ui_file_renamer as main_frame
+import app_lists
 
 
-MA_RESOLUTIONS = [
-    "1920x1080",
-    "1280x720",
-]
-
-
-MEZZ_RESOLUTIONS = [
-    "1280x720",
-    "720x480",
-]
-
-
-CUSTOM_RESOLUTIONS = [
-    "1920x1080",
-    "1280x720",
-    "720x480",
-]
+filename = OrderedDict([
+    ("title", ""),
+    ("reel", ""),
+    ("resolution", ""),
+    ("extension", ""),
+])
 
 
 class FileRenamerApp(QtGui.QMainWindow, main_frame.Ui_FileRenamerWindow):
@@ -45,33 +36,55 @@ class FileRenamerApp(QtGui.QMainWindow, main_frame.Ui_FileRenamerWindow):
         self.default_custom.toggled.connect(self.set_default_custom)
 
         self.resolution_combo.currentIndexChanged.connect(self.set_resolution)
-        self.resolution_combo.currentIndexChanged.connect(self.set_resolution)
-        self.resolution_combo.currentIndexChanged.connect(self.set_resolution)
+        self.extension_combo.currentIndexChanged.connect(self.set_extension)
 
         self.default_ma.toggle()
 
         self.show()
 
     def set_title(self):
-        self.results_line.insert(self.title_line.text())
+        filename["title"] = self.title_line.text()
+        self.set_results()
 
     def set_reel(self):
-        self.results_line.insert(self.title_line.text())
+        filename["reel"] = self.reel_line.text()
+        self.set_results()
 
     def set_default_ma(self):
         self.resolution_combo.clear()
-        self.resolution_combo.addItems(MA_RESOLUTIONS)
+        self.resolution_combo.addItems(app_lists.MA_RESOLUTIONS)
+        self.extension_combo.clear()
+        self.extension_combo.addItems(app_lists.CUSTOM_EXTENSIONS)
 
     def set_default_mezz(self):
         self.resolution_combo.clear()
-        self.resolution_combo.addItems(MEZZ_RESOLUTIONS)
+        self.resolution_combo.addItems(app_lists.MEZZ_RESOLUTIONS)
+        self.extension_combo.clear()
+        self.extension_combo.addItems(app_lists.CUSTOM_EXTENSIONS)
 
     def set_default_custom(self):
         self.resolution_combo.clear()
-        self.resolution_combo.addItems(CUSTOM_RESOLUTIONS)
+        self.resolution_combo.addItems([(tup[0]) for tup in app_lists.CUSTOM_RESOLUTIONS])
+        self.extension_combo.clear()
+        self.extension_combo.addItems(app_lists.CUSTOM_EXTENSIONS)
 
     def set_resolution(self):
-        pass
+        try:
+            filename["resolution"] = \
+            [y for x, y, z in app_lists.CUSTOM_RESOLUTIONS if x == self.resolution_combo.currentText()][0]
+            self.set_results()
+
+        except IndexError:
+            return
+
+    def set_extension(self):
+        filename["extension"] = self.extension_combo.currentText()
+        self.set_results()
+
+    def set_results(self):
+        self.results_line.clear()
+        for key in filename.keys():
+            self.results_line.insert(filename[key])
 
 
 def main():
