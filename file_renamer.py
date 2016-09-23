5d516157e2db822439646853df9cc168e6d89e24
Simon Lachaîne <simonthechain@gmail.com>
2016-09-23 01:37:50 -0400

    Need to add default check to set_resolution

--- file_renamer.py
+++ file_renamer.py
@@ -42,31 +42,89 @@ class FileRenamerApp(QtGui.QMainWindow, main_frame.Ui_FileRenamerWindow):
 
         self.show()
 
-    def set_title(self):
-        filename["title"] = self.title_line.text()
-        self.set_results()
-
-    def set_reel(self):
-        filename["reel"] = self.reel_line.text()
-        self.set_results()
-
     def set_default_ma(self):
+        temp_resolution = self.resolution_combo.currentText()
         self.resolution_combo.clear()
         self.resolution_combo.addItems(app_lists.MA_RESOLUTIONS)
+
+        if temp_resolution in [self.resolution_combo.itemText(i) for i in range(self.resolution_combo.count())]:
+            index_resolution = self.resolution_combo.findText(temp_resolution)
+            self.resolution_combo.setCurrentIndex(index_resolution)
+
+        temp_extension = self.extension_combo.currentText()
         self.extension_combo.clear()
         self.extension_combo.addItems(app_lists.CUSTOM_EXTENSIONS)
+
+        if temp_extension in [self.extension_combo.itemText(i) for i in range(self.extension_combo.count())]:
+            index_extension = self.extension_combo.findText(temp_extension)
+            self.extension_combo.setCurrentIndex(index_extension)
+
+        self.set_title()
+        self.set_reel()
 
     def set_default_mezz(self):
+        temp_resolution = self.resolution_combo.currentText()
         self.resolution_combo.clear()
         self.resolution_combo.addItems(app_lists.MEZZ_RESOLUTIONS)
+
+        if temp_resolution in [self.resolution_combo.itemText(i) for i in range(self.resolution_combo.count())]:
+            index_resolution = self.resolution_combo.findText(temp_resolution)
+            self.resolution_combo.setCurrentIndex(index_resolution)
+
+        temp_extension = self.extension_combo.currentText()
+        self.extension_combo.clear()
+        self.extension_combo.addItems(app_lists.CUSTOM_EXTENSIONS)
+        
+        if temp_extension in [self.extension_combo.itemText(i) for i in range(self.extension_combo.count())]:
+            index_extension = self.extension_combo.findText(temp_extension)
+            self.extension_combo.setCurrentIndex(index_extension)
+
+        self.set_title()
+        self.set_reel()
+
+    def set_default_custom(self):
+        temp_resolution = self.resolution_combo.currentText()
+        self.resolution_combo.clear()
+        self.resolution_combo.addItems([(tup[0]) for tup in app_lists.CUSTOM_RESOLUTIONS])
+
+        if temp_resolution in [self.resolution_combo.itemText(i) for i in range(self.resolution_combo.count())]:
+            index_resolution = self.resolution_combo.findText(temp_resolution)
+            self.resolution_combo.setCurrentIndex(index_resolution)
+
+        temp_extension = self.extension_combo.currentText()
         self.extension_combo.clear()
         self.extension_combo.addItems(app_lists.CUSTOM_EXTENSIONS)
 
-    def set_default_custom(self):
-        self.resolution_combo.clear()
-        self.resolution_combo.addItems([(tup[0]) for tup in app_lists.CUSTOM_RESOLUTIONS])
-        self.extension_combo.clear()
-        self.extension_combo.addItems(app_lists.CUSTOM_EXTENSIONS)
+        if temp_extension in [self.extension_combo.itemText(i) for i in range(self.extension_combo.count())]:
+            index_extension = self.extension_combo.findText(temp_extension)
+            self.extension_combo.setCurrentIndex(index_extension)
+
+        self.set_title()
+        self.set_reel()
+
+    def set_title(self):
+        if self.default_ma.isChecked():
+            filename["title"] = str(self.title_line.text()).upper().replace(" ", "") + "_"
+
+        elif self.default_mezz.isChecked():
+            filename["title"] = str(self.title_line.text()).lower().replace(" ", "")
+
+        elif self.default_custom.isChecked():
+            filename["title"] = self.title_line.text()
+
+        return self.set_results()
+
+    def set_reel(self):
+        if self.default_ma.isChecked():
+            filename["reel"] = str(self.reel_line.text()).replace(" ", "") + "_"
+
+        elif self.default_mezz.isChecked():
+            filename["reel"] = str(self.reel_line.text()).replace(" ", "")
+
+        elif self.default_custom.isChecked():
+            filename["reel"] = self.reel_line.text()
+
+        return self.set_results()
 
     def set_resolution(self):
         try:
@@ -79,12 +137,51 @@ class FileRenamerApp(QtGui.QMainWindow, main_frame.Ui_FileRenamerWindow):
 
     def set_extension(self):
         filename["extension"] = self.extension_combo.currentText()
-        self.set_results()
+
+        return self.set_results()
 
     def set_results(self):
         self.results_line.clear()
         for key in filename.keys():
             self.results_line.insert(filename[key])
+
+        if self.default_ma.isChecked():
+            self.validate_ma()
+
+        if self.default_mezz.isChecked():
+            self.validate_mezz()
+
+    def validate_ma(self):
+        self.validation_text.clear()
+
+        if filename["title"] == "_":
+            self.validation_text.appendPlainText("Title missing.")
+
+        if filename["reel"] == "_":
+            self.validation_text.appendPlainText("Reel # missing.")
+
+        else:
+            try:
+                int(filename["reel"].replace("_", ""))
+
+            except ValueError:
+                self.validation_text.appendPlainText("Reel # is not a valid number.")
+
+    def validate_mezz(self):
+        self.validation_text.clear()
+
+        if not filename["title"]:
+            self.validation_text.appendPlainText("Title missing.")
+
+        if not filename["reel"]:
+            self.validation_text.appendPlainText("Reel # missing.")
+
+        else:
+            try:
+                int(filename["reel"])
+
+            except ValueError:
+                self.validation_text.appendPlainText("Reel # is not a valid number.")
 
 
 def main():
